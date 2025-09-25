4647323deef20a5e17264c04ab9ce5eb2ea7e6df
Mykyta Vavulin <nik.vavulin@gmail.com>
2025-09-25 18:18:31 +0200

    Creé las los funciones listarGastos, anyadirGasto, borrarGasto, calcularTotalGastos y calcularBalance

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -3,6 +3,8 @@
 
 // TODO: Variable global
 let presupuesto = 0;
+let gastos = [];
+let idGasto = 0;
 
 function actualizarPresupuesto(nuevoPresupuesto) {
     if(!isNaN(nuevoPresupuesto) && nuevoPresupuesto >= 0)
@@ -21,7 +23,7 @@ function mostrarPresupuesto() {
     return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto(descripcion, valor) {
+function CrearGasto(descripcion, valor, fecha) {
     if (!isNaN(valor) && valor >= 0)
     {
         this.valor = valor;
@@ -30,7 +32,10 @@ function CrearGasto(descripcion, valor) {
     {
         this.valor = 0;
     }
+    
     this.descripcion = descripcion;
+    this.fecha = fecha;
+
 
     this.mostrarGasto = function()
     {
@@ -49,11 +54,40 @@ function CrearGasto(descripcion, valor) {
     }
 }
 
+function listarGastos()
+{
+    return gastos;
+}
+
+function anyadirGasto()
+{
+
+}
+
+function borrarGasto()
+{
+
+}
+
+function calcularTotalGastos()
+{
+
+}
+
+function calcularBalance()
+{
+
+}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
